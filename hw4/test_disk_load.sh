5ce91b6b24f56d88582ed1c366954a505ddee3c7
Angelos Kyriacou <angkyria@podilates.com>
2016-05-17 19:34:05 +0300

    testing script

--- hw4/test_disk_load.sh
+++ hw4/test_disk_load.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-if [ $@ -eq 10 ]; then
-    sudo dd if=/dev/zero of=/media/test_disk/test.img bs=1 count=0 seek=10M
-	sudo dd if=/dev/zero of=/media/test_disk/test1.img bs=1 count=0 seek=10M
-	sudo dd if=/dev/zero of=/media/test_disk/test2.img bs=1 count=0 seek=10M
-	sudo dd if=/dev/zero of=/media/test_disk/test3.img bs=1 count=0 seek=10M
-	sudo dd if=/dev/zero of=/media/test_disk/test4.img bs=1 count=0 seek=10M
-	sudo dd if=/dev/zero of=/media/test_disk/test5.img bs=1 count=0 seek=10M
+if [ $@ -eq 5 ]; then
+    for i in {1..600}
+    do
+        sudo dd if=/dev/zero of=/media/test_disk/test$i.img bs=1 count=0 seek=5M
+    done
 elif [ $@ -eq 100 ]; then
     sudo dd if=/dev/zero of=/media/test_disk/test.img bs=1 count=0 seek=100M
 elif [ $@ -eq 250 ]; then
@@ -15,6 +13,6 @@ elif [ $@ -eq 500 ]; then
     sudo dd if=/dev/zero of=/media/test_disk/test.img bs=1 count=0 seek=500M
 else
     echo "Wrong operation"
-	echo "Choose between 10, 100, 250, 500"
+	echo "Choose between 5, 100, 250, 500"
 	echo "Example ./test_disk_load.sh 250"
 fi
