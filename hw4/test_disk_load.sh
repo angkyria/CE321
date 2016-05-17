8fcac4e2e36cc373e2df8ce90d5b231d7fc39aae
Angelos Kyriacou <angkyria@podilates.com>
2016-05-17 19:02:04 +0300

    final testing script

--- hw4/test_disk_load.sh
+++ hw4/test_disk_load.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-dd if=/dev/zero of=test.img bs=1024 count=0 seek=$[1024*200]
+if [ $@ -eq 10 ]; then
+    dd if=/dev/zero of=/media/test_disk/test.img bs=1 count=0 seek=10M
+elif [ $@ -eq 100 ]; then
+    dd if=/dev/zero of=/media/test_disk/test.img bs=1 count=0 seek=100M 
+elif [ $@ -eq 250 ]; then
+    dd if=/dev/zero of=/media/test_disk/test.img bs=1 count=0 seek=250M 
+elif [ $@ -eq 500 ]; then
+    dd if=/dev/zero of=/media/test_disk/test.img bs=1 count=0 seek=500M 
+else
+    echo "Wrong operation"
+fi
